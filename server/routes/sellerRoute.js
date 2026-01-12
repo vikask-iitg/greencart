3d464410305b9aa3a5a95db5621a262431124307
vikask-iitg <vikask.iitg@gmail.com>
2026-01-12 13:22:49 +0530

    sellerController.js file name change

--- server/routes/sellerRoute.js
+++ server/routes/sellerRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { isSellerAuth, sellerLogin, sellerLogout } from '../controllers/SellerController.js';
+import { isSellerAuth, sellerLogin, sellerLogout } from '../controllers/sellerController.js';
 import authSeller from '../middlewares/authSeller.js';
 
 const sellerRouter = express.Router();
